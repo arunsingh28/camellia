8376a4b86cfd8320b26e7b599a39dfd09dfa9f14
Arun Singh <arunsingh28aug.as@gmail.com>
2025-02-21 18:47:11 +0530

    Add export functionality and refactor Master Data page layout

--- client/src/pages/master-data/index.tsx
+++ client/src/pages/master-data/index.tsx
@@ -1,12 +1,8 @@
-import { Button, Typography, Dropdown, Space } from 'antd';
+import { Button, Typography, Space } from 'antd';
 import {
     Database,
     UserIcon,
     GraduationCap,
-    ChevronDown,
-    Plus,
-    Pencil,
-    Trash,
     Upload,
     Download,
 } from 'lucide-react';
@@ -14,35 +10,6 @@ import { Link } from 'react-router-dom';
 
 import Statics from '@/pages/dashboard/components/Statics';
 import { paths } from '@/router/path';
-
-import type { MenuProps } from 'antd';
-
-const studentItems: MenuProps['items'] = [
-    {
-        key: '1',
-        label: 'Student Data',
-        disabled: false,
-    },
-    {
-        type: 'divider',
-    },
-    {
-        key: '2',
-        label: 'Add New Student Data',
-        icon: <Plus size={18} />,
-    },
-    {
-        key: '3',
-        label: 'Edit Existing Student Data',
-        icon: <Pencil size={18} />,
-    },
-    {
-        key: '4',
-        label: 'Delete Student Data',
-        icon: <Trash size={18} />,
-        className: '!text-red-700',
-    },
-];
 
 const MasterData = () => {
     return (
@@ -76,53 +43,97 @@ const MasterData = () => {
                         className="shadow-md"
                     />
                 </Link>
-                {/* <Link to={paths.APP.STAFF.INDEX}>
-                    <Statics
-                        icon={Users}
-                        title="Total Staff"
-                        value="5"
-                        prefix=""
-                        className="shadow-md"
-                    />
-                </Link> */}
             </div>
             <div>
                 <Typography.Paragraph className="!text-primary uppercase">
-                    Create New Data{' '}
+                    Import Data
                     <span className="italic text-gray-500">or</span> Edit Data
                 </Typography.Paragraph>
 
                 <div className="flex gap-4">
                     <div className="w-[500px] px-3 py-2 border rounded-lg bg-white shadow-md">
-                        <Typography.Title level={5}>Teacher Record</Typography.Title>
+                        <Typography.Title level={5}>
+                            Teacher Record
+                        </Typography.Title>
                         <Typography.Paragraph className="!text-gray-600">
-                            download template and upload bulk teacher record don't change the column name and format.
+                            download template and upload bulk teacher record
+                            don't change the column name and format.
                         </Typography.Paragraph>
                         <Space>
-                           <Button className='hover:!border-primary hover:!text-primary' icon={<Upload size={18} />} classNames={{ icon: 'mt-1.5' }}>Upload Bulk Teacher Record</Button>
-                           <Button className='hover:!border-primary hover:!text-primary' icon={<Download size={18} />} classNames={{ icon: 'mt-1.5' }}>Download Template</Button>
+                            <Button
+                                className="hover:!border-primary hover:!text-primary"
+                                icon={<Upload size={18} />}
+                                classNames={{ icon: 'mt-1.5' }}
+                            >
+                                Upload Bulk Teacher Record
+                            </Button>
+                            <Button
+                                className="hover:!border-primary hover:!text-primary"
+                                icon={<Download size={18} />}
+                                classNames={{ icon: 'mt-1.5' }}
+                            >
+                                Download Template
+                            </Button>
                         </Space>
                     </div>
                     <div className="w-[500px] px-3 py-2 border rounded-lg bg-white shadow-md">
-                        <Typography.Title level={5}>Upload Bulk Student Record</Typography.Title>
-                        <Typography.Paragraph className="!text-gray-600"> 
-                            download template and upload bulk student record don't change the column name and format.
+                        <Typography.Title level={5}>
+                            Upload Bulk Student Record
+                        </Typography.Title>
+                        <Typography.Paragraph className="!text-gray-600">
+                            download template and upload bulk student record
+                            don't change the column name and format.
                         </Typography.Paragraph>
-                          <Space>
-                          <Button className='hover:!border-primary hover:!text-primary'
-                            icon={<Upload size={18} />}
+                        <Space>
+                            <Button
+                                className="hover:!border-primary hover:!text-primary"
+                                icon={<Upload size={18} />}
                                 classNames={{ icon: 'mt-1.5' }}
                             >
                                 Upload Bulk Student Record
                             </Button>
-                            <Button className='hover:!border-primary hover:!text-primary'
-                            icon={<Download size={18} />}
+                            <Button
+                                className="hover:!border-primary hover:!text-primary"
+                                icon={<Download size={18} />}
                                 classNames={{ icon: 'mt-1.5' }}
                             >
-                               Download Template
+                                Download Template
                             </Button>
-                     
-                          </Space>
+                        </Space>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-5">
+                <Typography.Paragraph className="!text-primary uppercase">
+                    export data
+                </Typography.Paragraph>
+
+                <div className="flex gap-4">
+                    <div className="w-[400px] px-3 py-3 border rounded-lg bg-white shadow-md">
+                        <Typography.Title level={5}>
+                            Teacher Record
+                        </Typography.Title>
+
+                        <Button
+                            className="hover:!border-primary hover:!text-primary my-2"
+                            icon={<Download size={18} />}
+                            classNames={{ icon: 'mt-1.5' }}
+                        >
+                            Download
+                        </Button>
+                    </div>
+                    <div className="w-[400px] px-3 py-3 border rounded-lg bg-white shadow-md">
+                        <Typography.Title level={5}>
+                            Upload Bulk Student Record
+                        </Typography.Title>
+
+                        <Button
+                            className="hover:!border-primary hover:!text-primary my-2"
+                            icon={<Download size={18} />}
+                            classNames={{ icon: 'mt-1.5' }}
+                        >
+                            Download
+                        </Button>
                     </div>
                 </div>
             </div>
