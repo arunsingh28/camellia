9bac0a3f0d901da0fdcb5869a46a20673fc130eb
Arun Singh <arunsingh28aug.as@gmail.com>
2025-03-27 13:50:10 +0530

    Add custom useDebounce hook for improved input handling

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -2,12 +2,15 @@ import { menuOptions, IMenuOptions } from '@/utils/menu';
 import { Link, useLocation } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import { Input } from 'antd';
-import { LifeBuoy, SearchIcon } from 'lucide-react';
+import { SearchIcon } from 'lucide-react';
 import { cn } from '@/utils/util';
+
+import useDebounce from '@/hooks/useDebounce';
 
 const Navbar = () => {
     const location = useLocation();
     const [search, setSearch] = useState('');
+    const debouncedSearch = useDebounce(search, 300);
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
@@ -16,25 +19,26 @@ const Navbar = () => {
     const [menu, setMenu] = useState<IMenuOptions>(menuOptions);
 
     useEffect(() => {
-        const filteredMenu = {
-            main: menuOptions.main.filter((item) =>
-                item.name.toLowerCase().includes(search.toLowerCase()),
-            ),
-            financing: menuOptions.financing.filter((item) =>
-                item.name.toLowerCase().includes(search.toLowerCase()),
-            ),
-            utils: menuOptions.utils.filter((item) =>
-                item.name.toLowerCase().includes(search.toLowerCase()),
-            ),
-        };
+        const filteredMenu = Object.keys(menuOptions).reduce((acc, key) => {
+            acc[key] = menuOptions[key].filter((item) =>
+                item.name.toLowerCase().includes(debouncedSearch.toLowerCase()),
+            );
+            return acc;
+        }, {} as IMenuOptions);
 
         setMenu(filteredMenu);
-    }, [search]);
+    }, [debouncedSearch]);
 
     const handleResetSearch = () => {
         setSearch('');
         setMenu(menuOptions);
     };
+
+    const menuSections = [
+        { title: 'MAIN', items: menu.main },
+        { title: 'FINANCING', items: menu.financing },
+        { title: 'UTILS', items: menu.utils },
+    ];
 
     return (
         <React.Fragment>
@@ -50,112 +54,20 @@ const Navbar = () => {
                         className="mx-2 w-[calc(100%-1rem)] rounded-md px-2 py-1 border border-gray-300 mb-4 text-sm focus:!border-primary focus:!ring-primary focus:!ring-1 active:!border-primary active:!ring-primary focus:!outline-none"
                         onChange={handleSearch}
                     />
-                    {menu.main.length > 0 && (
-                        <div className='pr-2'>
-                            {menu.main.map((item) => (
-                                <Link
-                                    to={item.path}
-                                    className={cn(
-                                        'flex items-center gap-2 my-2 cursor-pointer px-2 py-1.5 hover:bg-gray-200 border-l-[3px] rounded-none group',
-                                        location.pathname === item.path
-                                            ? 'bg-gray-200 border-l-[3px] border-primary rounded-r-md transition-all duration-300'
-                                            : 'border-l-transparent rounded-md transition-all duration-300 hover:!ml-2',
-                                    )}
-                                    onClick={handleResetSearch}
-                                    key={item.path}
-                                >
-                                    <item.icon
-                                        size={16}
-                                        className={cn(
-                                            'group-hover:text-primary group-hover:stroke-primary',
-                                            location.pathname === item.path
-                                                ? 'text-primary stroke-primary'
-                                                : 'text-gray-700 stroke-gray-700',
-                                        )}
-                                    />
-                                    <p className="text-sm text-gray-700">
-                                        {item.name}
-                                    </p>
-                                </Link>
-                            ))}
-                        </div>
-                    )}
+                    {menuSections.map((section, index) => (
+                        <MenuSection
+                            key={index}
+                            title={
+                                section.title === 'MAIN'
+                                    ? undefined
+                                    : section.title
+                            }
+                            items={section.items}
+                            handleResetSearch={handleResetSearch}
+                            location={location}
+                        />
+                    ))}
 
-                    {menu.financing.length > 0 && (
-                        <>
-                            <div className="w-[calc(100%-1rem)] h-[1px] bg-gray-200 my-3 mt-6 ml-2" />
-
-                            <div className='pr-2'>
-                                <span className="text-[12px] font-roboto text-primary mb-3 ml-2">
-                                    FINANCING
-                                </span>
-                                {menu.financing.map((item) => (
-                                    <Link
-                                        to={item.path}
-                                        className={cn(
-                                            'flex items-center gap-2 my-2 cursor-pointer px-2 py-1.5 hover:bg-gray-200 border-l-[3px] rounded-none group',
-                                            location.pathname === item.path
-                                                ? 'bg-gray-200 border-l-[3px] border-primary rounded-r-md transition-all duration-300'
-                                                : 'border-l-transparent rounded-md transition-all duration-300 hover:!ml-2',
-                                        )}
-                                        onClick={handleResetSearch}
-                                        key={item.path}
-                                    >
-                                        <item.icon
-                                            size={16}
-                                            className={cn(
-                                                'group-hover:text-primary group-hover:stroke-primary',
-                                                location.pathname === item.path
-                                                    ? 'text-primary stroke-primary'
-                                                    : 'text-gray-700 stroke-gray-700',
-                                            )}
-                                        />
-                                        <p className="text-sm text-gray-700">
-                                            {item.name}
-                                        </p>
-                                    </Link>
-                                ))}
-                            </div>
-                        </>
-                    )}
-                    {menu.utils.length > 0 && (
-                        <>
-                            <div className="w-[calc(100%-1rem)] h-[1px] bg-gray-200 my-3 mt-6 ml-2" />
-                            <div className='pr-2'>
-                                <p className="text-[12px] font-roboto text-primary mb-3 ml-2">
-                                    UTILS
-                                </p>
-                                {menu.utils &&
-                                    menu.utils.map((item) => (
-                                        <Link
-                                            to={item.path}
-                                            className={cn(
-                                                'flex items-center gap-2 my-2 cursor-pointer px-2 py-1.5 hover:bg-gray-200 border-l-[3px] rounded-none group',
-                                                location.pathname === item.path
-                                                    ? 'bg-gray-200 border-l-[3px] border-primary rounded-r-md transition-all duration-300'
-                                                    : 'border-l-transparent rounded-md transition-all duration-300 hover:!ml-2',
-                                            )}
-                                            onClick={handleResetSearch}
-                                            key={item.path}
-                                        >
-                                            <item.icon
-                                                size={16}
-                                                className={cn(
-                                                    'group-hover:text-primary group-hover:stroke-primary',
-                                                    location.pathname ===
-                                                        item.path
-                                                        ? 'text-primary stroke-primary'
-                                                        : 'text-gray-700 stroke-gray-700',
-                                                )}
-                                            />
-                                            <p className="text-sm text-gray-700">
-                                                {item.name}
-                                            </p>
-                                        </Link>
-                                    ))}
-                            </div>
-                        </>
-                    )}
                     {Object.values(menu).every((menu) => menu.length === 0) && (
                         <p className="text-gray-500 text-xs font-roboto py-5 px-2">
                             No results found
@@ -163,48 +75,82 @@ const Navbar = () => {
                     )}
                 </div>
                 <div>
-                    <div className="border p-2 rounded-md text-sm gap-2 bg-gray-100 font-roboto mx-2">
-                        <p className="flex items-center gap-2 text-gray-600 text-[13px]">
-                            <LifeBuoy size={16} />
-                            Need Help ?
-                        </p>
-                        <p className="text-[12px]">
-                            <span className="text-primary font-medium">
-                                Get in touch
-                            </span>{' '}
-                            with one of our experts.
-                        </p>
-                    </div>
                     <div className="w-full h-[1px] bg-gray-200 my-3" />
-
-                    <div className='pr-2'>
-                        <p className="text-[12px] font-roboto text-primary mb-3 uppercase ml-2">
-                            genreal
-                        </p>
-                        {menuOptions.general.map((item) => (
-                            <Link
-                                to={item.path}
-                                className={
-                                    'flex items-center gap-2 my-1 cursor-pointer px-2 py-1.5 rounded-md hover:bg-white group transition-all duration-300 hover:!ml-2'
-                                }
-                                key={item.path}
-                            >
-                                <item.icon
-                                    size={16}
-                                    className={
-                                        'group-hover:text-red-700 group-hover:stroke-red-700'
-                                    }
-                                />
-                                <p className="text-sm text-gray-700 group-hover:text-red-700">
-                                    {item.name}
-                                </p>
-                            </Link>
-                        ))}
-                    </div>
+                    <MenuSection
+                        title="GENERAL"
+                        items={menuOptions.general}
+                        handleResetSearch={handleResetSearch}
+                        location={location}
+                        isGeneral
+                    />
                 </div>
             </div>
         </React.Fragment>
     );
 };
 
-export default Navbar;
+export default React.memo(Navbar);
+
+interface MenuSectionsProps {
+    title?: string;
+    items: IMenuOptions[keyof IMenuOptions];
+    handleResetSearch: () => void;
+    location: ReturnType<typeof useLocation>;
+    isGeneral?: boolean;
+}
+
+const MenuSection: React.FC<MenuSectionsProps> = ({
+    title,
+    items,
+    handleResetSearch,
+    location,
+    isGeneral = false,
+}) => {
+    if (items.length === 0) return null;
+    return (
+        <div className="pr-2">
+            {title && (
+                <p className="text-[12px] font-roboto text-primary mb-3 ml-2 uppercase">
+                    {title}
+                </p>
+            )}
+            {items.map((item) => (
+                <Link
+                    to={item.path}
+                    className={cn(
+                        'flex items-center gap-2 my-2 cursor-pointer px-2 py-1.5 hover:bg-gray-200 border-l-[3px] rounded-none group',
+                        location.pathname === item.path
+                            ? 'bg-gray-200 border-l-[3px] border-primary rounded-r-md transition-all duration-300'
+                            : 'border-l-transparent rounded-md transition-all duration-300 hover:!ml-2',
+                    )}
+                    onClick={handleResetSearch}
+                    key={item.path}
+                >
+                    <item.icon
+                        size={16}
+                        className={cn(
+                            isGeneral
+                                ? 'group-hover:text-red-700 group-hover:stroke-red-700'
+                                : 'group-hover:text-primary group-hover:stroke-primary',
+                            location.pathname === item.path
+                                ? isGeneral
+                                    ? 'text-red-700 stroke-red-700'
+                                    : 'text-primary stroke-primary'
+                                : 'text-gray-700 stroke-gray-700',
+                        )}
+                    />
+                    <p
+                        className={cn(
+                            'text-sm',
+                            isGeneral
+                                ? 'text-gray-700 group-hover:text-red-700'
+                                : 'text-gray-700',
+                        )}
+                    >
+                        {item.name}
+                    </p>
+                </Link>
+            ))}
+        </div>
+    );
+};
